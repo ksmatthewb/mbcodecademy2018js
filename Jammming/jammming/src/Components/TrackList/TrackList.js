176b8644d6f42c92bb0e74579bfebdded2990fdb
ksmatthewb <38468494+ksmatthewb@users.noreply.github.com>
2018-06-13 10:14:00 -0500

    Add files via upload

---
+++ Jammming/jammming/src/Components/TrackList/TrackList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+  render(){
+    return (
+        <div className="TrackList">
+          {this.props.tracks && this.props.tracks.map(eachtrack => {
+          return (
+            <Track track={eachtrack} key={eachtrack.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />
+          );
+        })}
+        </div>
+    );
+  }
+}
+export default TrackList;
